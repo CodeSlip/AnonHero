30a34a70c68179cf358db8cde6f9eb153edd9206
michaelcohen716 <michaelcohen716@gmail.com>
2019-09-07 13:18:58 -0400

    add basic contract

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 
+let HDWalletProvider = require("truffle-hdwallet-provider");
+let privateKey = "1E25C8731DE51F919A23EF70749251BB4F57D80BFD6468FC450FD79D39E3B87C"; // testnet
+let skale = "http://ethboston1.skalenodes.com:10145";
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +12,11 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    skale: {
+      provider: () => new HDWalletProvider(privateKey, skale),
+      gasPrice: 0,
+      network_id: "*"
     }
   }
 };
