7134e6d3c5e6cfe17841da039b587ffe60b778cd
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-09-07 15:40:55 -0400

    setting location

--- client/src/Map/Map.js
+++ client/src/Map/Map.js
@@ -1,38 +1,40 @@
 import React from 'react';
-
-
-
 import {Component} from 'react';
 import ReactMapGL from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+import './Map.css';
+import { Button } from 'reactstrap';
+
+
 class Map extends Component {
-
-  state = {
-    viewport: {
-      width: 400,
-      height: 400,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
+    constructor(props){
+        super(props);
+        this.state = {
+            viewport: {
+              width: '100%',
+              height: '100%',
+              latitude: 37.7577,
+              longitude: -122.4376,
+              zoom: 8
+            }
+        };
     }
-  };
 
-  render() {
-    return (
-        <div>
-            <div className="page-header">
-                <h4>AnonHero | Map</h4>
+
+
+    render() {
+        return (
+            <div className='map-view' style={{height: 'calc(100% - 90px)', width: '100%'}}>
+                <ReactMapGL
+                    mapboxApiAccessToken = 'pk.eyJ1IjoidGlmZmFueW1xIiwiYSI6ImNrMDl3a2p3cjBkZGYzbW55djZ4NDgzMzcifQ.GcKDVp7Hzst2xXfpldKGcg'
+                    {...this.state.viewport}
+                    onViewportChange={(viewport) => this.setState({viewport})}
+                />
+                <Button className="map-submit-btn" onClick={this.setLocation} >Set Location</Button>
             </div>
-            <ReactMapGL
-                mapboxApiAccessToken = 'pk.eyJ1IjoidGlmZmFueW1xIiwiYSI6ImNrMDl3a2p3cjBkZGYzbW55djZ4NDgzMzcifQ.GcKDVp7Hzst2xXfpldKGcg'
-                {...this.state.viewport}
-                onViewportChange={(viewport) => this.setState({viewport})}
-            />
-        </div>
-  
-    );
-  }
+        );
+    }
 }
 
 
