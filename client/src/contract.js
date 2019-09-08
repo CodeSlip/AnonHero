ed9d3f9850411f9b4af85908986bc2f7597be398
michaelcohen716 <michaelcohen716@gmail.com>
2019-09-07 22:37:39 -0400

    load events

--- client/src/contract.js
+++ client/src/contract.js
@@ -1,10 +1,12 @@
 import Web3 from "web3";
 import AnonAbi from "./contracts/Anon.json";
 import web3Obj from "./helper";
-// const web3Provider = new Web3.providers.HttpProvider(
-//   "http://ethboston1.skalenodes.com:10145"
-// );
-// let web3 = new Web3(web3Provider);
+console.log("web3obj", web3Obj)
+const web3Provider = new Web3.providers.HttpProvider(
+  "https://ethboston1.skalenodes.com:10150"
+);
+export const web3 = new Web3(web3Provider);
+const Tx = require('ethereumjs-tx');
 
 const SkaleAnonContract = "0x6F29BA25Fab3d81616A79De20736A58813C6c3E7";
 
@@ -12,14 +14,35 @@ async function Contract() {
   return await new web3Obj.web3.eth.Contract(AnonAbi.abi, SkaleAnonContract);
 }
 
+async function ReadContract() {
+  return await new web3.eth.Contract(AnonAbi.abi, SkaleAnonContract);
+}
+
 export async function _createEvent() {
-    console.log("in create event")
   const contr = await Contract();
-  await contr.methods.createEvent("42.3792848", "-71.1156926", "Protest event");
+  const lat = web3Obj.web3.utils.fromAscii("42.3792848")
+  const long = web3Obj.web3.utils.fromAscii("-71.1156926")
+  const name = web3Obj.web3.utils.fromAscii("Protest for Justice")
+
+  const func = contr.methods.createEvent(lat, long, name).encodeABI();
+  web3Obj.web3.eth.getAccounts((err, res) => {
+    console.log("res", res)
+    web3Obj.web3.eth.sendTransaction(
+      {
+        from: res[0],
+        to: SkaleAnonContract,
+        data: func
+      }, ((error, txHash) => {
+        console.log("error", error)
+        console.log("txhash", txHash)
+      })
+    )
+  })
 }
 
 export async function _getEvent(eventId) {
-  const contr = await Contract();
-  const evt = await contr.methods.getEvent(eventId)
-  console.log("evt", evt)
+  const contr = await ReadContract();
+  console.log("contr", contr)
+  const evt = await contr.methods.getEvent(eventId).call()
+  return evt;
 }
