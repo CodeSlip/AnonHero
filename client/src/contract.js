49f2298a6c49e71efe559d134478f4d2c8768689
CodeSlip <jason@rawlings.io>
2019-09-08 09:12:41 -0400

    added smart contract

--- client/src/contract.js
+++ client/src/contract.js
@@ -1,14 +1,23 @@
 import Web3 from "web3";
 import AnonAbi from "./contracts/Anon.json";
+import AnonValidateAbi from "./contracts/AnonValidate.json";
 import web3Obj from "./helper";
 console.log("web3obj", web3Obj)
+//new route where ML & Storage are located
+
 const web3Provider = new Web3.providers.HttpProvider(
   "https://ethboston1.skalenodes.com:10150"
 );
-export const web3 = new Web3(web3Provider);
-const Tx = require('ethereumjs-tx');
 
+const web3ProviderB = new Web3.providers.HttpProvider(
+  "157.245.138.108"
+);
+
+export const web3 = new Web3(web3Provider);
+export const web3B = new Web3(web3ProviderB);
+const Tx = require('ethereumjs-tx');
 const SkaleAnonContract = "0x6F29BA25Fab3d81616A79De20736A58813C6c3E7";
+const SkaleAnonValidateContract = "";
 
 async function Contract() {
   return await new web3Obj.web3.eth.Contract(AnonAbi.abi, SkaleAnonContract);
@@ -18,15 +27,17 @@ async function ReadContract() {
   return await new web3.eth.Contract(AnonAbi.abi, SkaleAnonContract);
 }
 
+async function ValidateContract() {
+  return await new web3Obj.web3.eth.Contract(AnonAbi.abi, SkaleAnonValidateContract);
+}
+
 export async function _createEvent() {
   const contr = await Contract();
   const lat = web3Obj.web3.utils.fromAscii("42.3792848")
   const long = web3Obj.web3.utils.fromAscii("-71.1156926")
   const name = web3Obj.web3.utils.fromAscii("Protest for Justice")
-
   const func = contr.methods.createEvent(lat, long, name).encodeABI();
   web3Obj.web3.eth.getAccounts((err, res) => {
-    console.log("res", res)
     web3Obj.web3.eth.sendTransaction(
       {
         from: res[0],
@@ -42,13 +53,11 @@ export async function _createEvent() {
 
 export async function _createPost(latitude, longitude, filePath){
   const contr = await Contract();
-
   const lat = web3Obj.web3.utils.fromAscii(latitude)
   // const lat = web3Obj.web3.utils.fromAscii("42.3792848")
   const long = web3Obj.web3.utils.fromAscii(longitude)
   // const long = web3Obj.web3.utils.fromAscii("-71.1156926")
   const eventId = 1;
-
   const func = contr.methods.createPost(lat, long, eventId, filePath).encodeABI();
   web3Obj.web3.eth.getAccounts((err, res) => {
     console.log("res", res)
@@ -77,4 +86,13 @@ export async function _getPost(postId) {
   console.log("contr", contr)
   const evt = await contr.methods.getPost(postId).call()
   return evt;
+}
+
+export async function validate(address, fileName) {
+  //receives address of stored object, and name of stored object
+  const validationContr = await ValidateContract();
+  console.log('validation begins');
+  let result = await validationContr.methods.validate(address, fileName).call({gas: 100000000, gasPrice: 0});
+  console.log('validation completed');
+  return result;
 }
